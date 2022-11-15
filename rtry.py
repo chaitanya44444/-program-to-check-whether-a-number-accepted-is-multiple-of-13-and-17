0255c22c32b15c24368542977c8dfeaffb5cc741
chaitanya44444 <93338022+chaitanya44444@users.noreply.github.com>
2022-11-15 16:12:00 +0530

    Create rtry.py

---
+++ rtry.py
@@ -0,0 +1,5 @@
+num=int(input("write a number to see if its a multiple of 17 and 13:"))
+if num%221 == 0:
+        print(num,"is a multiple of 17 and 13")
+else:
+    print(num,"is not a multiple of 17 and 13")
